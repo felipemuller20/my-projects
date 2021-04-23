d7c764a0e94610cb8600245d4cdbdf0f683863a9
Felipe Muller <felipeavmuller@gmail.com>
2021-04-23 19:01:09 -0300

    Copying this project

---
+++ projects/jest-test-project/test/asyncJest.spec.js
@@ -0,0 +1,26 @@
+// const assert = require('assert');
+// const { hasUncaughtExceptionCaptureCallback } = require('process');
+const answerPhone = require('../src/asyncJest');
+/*
+A função answerPhone recebe um parâmetro boleano.
+Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
+
+Complete o código abaixo para testar as situações em que
+a função recebe como parâmetro true e false, respectivamente.
+
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+*/
+
+describe('o retorno do telefonema', () => {
+  test('atende', async () => {
+    const answer = await answerPhone(true);
+    expect(answer).toBe('Oi!');
+  });
+  test('ocupado', async () => {
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error).toBe(error);
+    }
+  });
+});
